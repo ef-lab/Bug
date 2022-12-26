7c66605f615ba82e6bbf4943219e0e3c6482502b
eman <olakiril@hotmail.com>
2022-12-26 12:52:04 +0200

    room_id

--- report.py
+++ report.py
@@ -16,14 +16,14 @@ while True:
     if log_timer.elapsed_time() > log_interval*1000:
         # log temp, hum, luminance
         tmst = str(datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
-        key = dict(room=room, tmst=tmst, celcius=ths.get_temperature())
+        key = dict(room_id=room, tmst=tmst, celcius=ths.get_temperature())
         logger.log('Temperature', key, schema='monitoring', priority=10)
 
-        key = dict(room=room, tmst=tmst, relative_humidity=ths.get_humidity())
+        key = dict(room_id=room, tmst=tmst, relative_humidity=ths.get_humidity())
         logger.log('Humidity', key, schema='monitoring', priority=10)
 
         RL, GL, BL = cam.estimate_channel_luminance()
-        key = dict(room=room, tmst=tmst, R_luminance=RL, G_luminance=GL, B_luminance=BL, trigger='time')
+        key = dict(room_id=room, tmst=tmst, R_luminance=RL, G_luminance=GL, B_luminance=BL, trigger='time')
         logger.log('Light', key, schema='monitoring', priority=10)
 
         # restart timer
@@ -36,16 +36,16 @@ while True:
         im = cam.snapshot()
 
         # log image
-        key = dict(room=room, tmst=tmst, image=im, trigger='motion')
+        key = dict(room_id=room, tmst=tmst, image=im, trigger='motion')
         logger.log('Camera', key, schema='monitoring', priority=10)
 
         # log motion
-        key = dict(room=room, tmst=tmst)
+        key = dict(room_id=room, tmst=tmst)
         logger.log('Motion', key, schema='monitoring', priority=10)
 
     if cam.light_change_detected():
         RL, GL, BL = cam.estimate_channel_luminance()
-        key = dict(room=room, tmst=tmst, R_luminance=RL, G_luminance=GL, B_luminance=BL, trigger='light')
+        key = dict(room_id=room, tmst=tmst, R_luminance=RL, G_luminance=GL, B_luminance=BL, trigger='light')
         logger.log('Light', key, schema='monitoring', priority=10)
 
     time.sleep(.1)
